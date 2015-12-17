891b690b170f4c2b75c0d8d3a904a55bfa1aab64
Rúni Egholm Vørmadal <runivormadal@outlook.com>
2015-12-17 13:56:49 +0100

    missing file

---
+++ YADrone/src/dtu/cdio/tutorial/listeners/PressureListenerImpl.java
@@ -0,0 +1,25 @@
+package dtu.cdio.tutorial.listeners;
+
+import de.yadrone.base.navdata.KalmanPressureData;
+import de.yadrone.base.navdata.Pressure;
+import de.yadrone.base.navdata.PressureListener;
+import dtu.cdio.tutorial.gui.DroneVariablesGUI;
+
+public class PressureListenerImpl implements PressureListener {
+
+	private DroneVariablesGUI gui;
+	public PressureListenerImpl(DroneVariablesGUI gui) {
+		this.gui = gui;
+	}
+
+	@Override
+	public void receivedKalmanPressure(KalmanPressureData d) {
+		// TODO
+	}
+
+	@Override
+	public void receivedPressure(Pressure d) {
+		if(gui != null) gui.setPressure(d.getValue() + "/" + d.getMeasurement());
+	}
+
+}
